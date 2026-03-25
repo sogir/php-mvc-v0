6528e43f779a432c65ed40a9f0dbe7222f0df9d0
sogir <sogir@ridwa.com>
2026-03-25 15:09:20 +0600

    Add Controllers

---
+++ app/Controllers/AboutController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controllers;
+
+class AboutController
+{
+     public function index(): string
+     {
+          return 'About';
+     }
+
+     public function contact(): string
+     {
+          return 'Contact';
+     }
+}
---
+++ app/Routes/RouteNotFoundException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Routes;
+
+class RouteNotFoundException extends \Exception 
+{
+     protected $message = '404 not found';
+}
